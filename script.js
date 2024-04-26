0e644674c59a81db59c74d3eb107ea2a8e3c4853
JhonatasMJ <jhonatasjhmj@gmail.com>
2024-04-25 21:25:09 -0300

    Conclusão Projeto

--- script.js
+++ script.js
@@ -13,26 +13,3 @@ if (
     }, 3500);
   });
 }
-
-// Verifica se o navegador suporta scroll suave
-if ("scrollBehavior" in document.documentElement.style) {
-  // Se suporta, adiciona a classe 'smooth-scroll' ao body
-  document.body.classList.add("smooth-scroll");
-} else {
-  // Se não suporta, adiciona a classe 'no-smooth-scroll' ao body
-  document.body.classList.add("no-smooth-scroll");
-}
-
-// Função para verificar se o scroll suave é suportado
-function scrollToTop() {
-  if ("scrollBehavior" in document.documentElement.style) {
-    // Se suporta, faz o scroll suave para o topo
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  } else {
-    // Se não suporta, faz o scroll padrão para o topo
-    window.scrollTo(0, 0);
-  }
-}
